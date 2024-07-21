27b349ad498061367668fe92540cf0139d6f9f86
Maxiflexy <onyemax247@gmail.com>
2024-07-21 20:03:34 +0100

    updated congig

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -11,8 +11,9 @@ const JobsListing = ({ isHome = false }) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
+      const apiUrl = isHome ? "/api/jobs?_limit=3" : "api/jobs";
       try {
-        const res = await fetch("http://localhost:8000/jobs");
+        const res = await fetch(apiUrl);
         const data = await res.json();
         setJobs(data);
       } catch (error) {
