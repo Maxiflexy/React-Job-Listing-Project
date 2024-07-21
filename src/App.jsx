c284b4616ae8fa2a145e2c84fb07e031bc1b7917
Maxiflexy <onyemax247@gmail.com>
2024-07-21 13:59:18 +0100

    created JobListing  component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
-import JobsListing from "./components/JobsListing";
+import JobsListing from "./components/JobListings";
 import Navbar from "./components/Navbar";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 const App = () => {
   return (
@@ -11,13 +12,7 @@ const App = () => {
     <HomeCards />
     <JobsListing />
 
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
+    <ViewAllJobs />
     </>
   )
 }
